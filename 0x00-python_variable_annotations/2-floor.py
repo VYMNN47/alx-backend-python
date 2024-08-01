39b33484db1c5152f376ee84adc82d783108d0ba
aymnn <ayman.belahsen.4@gmail.com>
2024-08-01 16:26:16 +0100

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Module for Floor Function"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+      type-annotated func which takes a float n as argument and
+      returns the floor of n.
+    """
+    return math.floor(n)
