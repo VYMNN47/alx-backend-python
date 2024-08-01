3fe13fea5ab7b2a49f39536dfaf2cc482ff8aa9c
aymnn <ayman.belahsen.4@gmail.com>
2024-08-01 16:22:47 +0100

    concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module for Concat Function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+      type-annotated function concat that takes string str1 and string str2
+      as arguments and returns a concatenated string
+    """
+    return str1 + str2
