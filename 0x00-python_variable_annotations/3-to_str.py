02c8d0cd0c1a98eb198560d87e9cbc61384c7416
aymnn <ayman.belahsen.4@gmail.com>
2024-08-01 16:28:45 +0100

    to_str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module for to_str Function"""
+
+
+def to_str(n: float) -> str:
+    """
+     type-annotated func that takes a float n as argument and
+     returns the string representation of n.
+    """
+    return f"{n}"
